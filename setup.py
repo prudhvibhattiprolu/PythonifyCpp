1a639f7709b86a1d66ece5684ca62cb3488dd84c
prudhvibhattiprolu <prudhvibhattiprolu@gmail.com>
2021-12-22 15:53:14 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+#To build:
+# >>> python setup.py --quiet build_ext --inplace clean --all
+
+
+import setuptools
+from distutils.core import setup
+from distutils.extension import Extension
+
+with open("README.md", "r") as README:
+    long_description = README.read()
+
+PowerPy = Extension(
+    'PowerPy',
+    sources=['src/PowerPy.cc'],
+    libraries=['boost_python38'],
+)
+
+setup(
+    name='PowerPy',
+    version='1.0',
+    author="Prudhvi Bhattiprolu",
+    author_email="prudhvibhattiprolu@gmail.com",
+    description="A working example of exposing C++(11) functions to Python using Boost-Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    ext_modules=[PowerPy]
+    )
---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+from .PowerPy import square as _square
+from .PowerPy import cube as _cube
+
+def square(x):
+    '''
+    Return square of a number
+    '''
+    return _square(x)
+
+def cube(x):
+    '''
+    Return cube of a number
+    '''
+    return _cube(x)
